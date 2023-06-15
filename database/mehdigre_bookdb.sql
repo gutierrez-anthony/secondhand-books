fb5fc2386990b4644718373af30bdab4f680a722
Mehdi Jokar <jokar.mehdi2@gmail.com>
2023-06-15 00:13:01 -0400

    Update database sql file

--- database/mehdigre_bookdb.sql
+++ database/mehdigre_bookdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 14, 2023 at 05:13 PM
+-- Generation Time: Jun 15, 2023 at 12:12 AM
 -- Server version: 10.2.44-MariaDB
 -- PHP Version: 8.1.16
 
@@ -52,10 +52,11 @@ INSERT INTO `Book` (`book_id`, `title`, `owner`, `authors`, `edition`, `descript
 (2, 'PHP and MySQL for Dynamic Web Sites', 2, 'Larry Ullman', '', 'Learn PHP and MySQL programmingâ€” the quick and easy way! Easy visual approach uses demonstrations and real-world examples to guide you step by step through advanced techniques for dynamic Web development using PHP and MySQL.', 'Software Development', 'uploads/648895458c191php-mysql.jpg', '648895458c191php-mysql.jpg', 15.00, 1, '12:11:49'),
 (3, 'Intro to Python for Computer Science and Data Science', 2, 'Paul Deitel and Harvey M. Deitel', '', 'Introduction to Python for Computer Science and Data Science takes a unique, modular approach to teaching and learning introductory Python programming that is relevant for both computer science and data science audiences. The Deitels cover the most current topics and applications to prepare you for your career. Jupyter Notebooks supplements provide opportunities to test your programming skills. Fully implemented case studies in artificial intelligence technologies and big data let you apply your knowledge to interesting projects in the business, industry, government and academia sectors. Hundreds of hands-on examples, exercises and projects offer a challenging and entertaining introduction to Python and data science.', 'Software Development', 'uploads/648896ec0c9a0intro-to-python.jpg', '648896ec0c9a0intro-to-python.jpg', 50.00, 1, '12:18:52'),
 (4, 'C# & C++: 5 Books in 1 - The #1 Coding Course from Beginner to Advanced', 2, 'Mark Reed', '', 'Learn the basic functions of C# and C++ Programming and progress on to advanced levels. Immerse yourself in the wealth of notions, exercises and practical examples made easily digestible for effortless learning and prompt gratification.', 'Software Development', 'uploads/6488987c1eb35c_and_c-plus-plus.jpg', '6488987c1eb35c_and_c-plus-plus.jpg', 15.00, 1, '12:25:32'),
-(5, 'Labore qui assumenda', 6, 'Eligendi cum mollit ', NULL, 'Aliquam quos velit s', 'Alias vel iure sunt ', 'uploads/648525203221961mIq2iJUXL.jpg', '648525203221961mIq2iJUXL.jpg', 585.00, 1, '21:36:32'),
+(5, 'Cracking the code interview', 6, 'Eligendi cum mollit ', '6th', 'Gayle Laakmann McDowell', 'Alias vel iure sunt ', 'uploads/648525203221961mIq2iJUXL.jpg', '648525203221961mIq2iJUXL.jpg', 25.35, 1, '21:36:32'),
 (6, 'Cupidatat illo labor', 6, 'Mollitia facere fugi', NULL, 'Quibusdam laboriosam', 'Vero rerum officia o', 'uploads/64852530863f181Gt2O4FvTL.jpg', '64852530863f181Gt2O4FvTL.jpg', 679.00, 1, '21:36:48'),
 (7, 'Et mollit aut minim ', 6, 'Dolore alias aliquip', NULL, 'Rerum error omnis se', 'Quam quasi iure cons', 'uploads/648525472358b91Ss-Th23nL.jpg', '648525472358b91Ss-Th23nL.jpg', 995.00, 1, '21:37:11'),
-(8, 'In voluptates illo v', 6, 'In dolores et nulla ', NULL, 'Ut corporis fugiat a', 'Maxime ipsa volupta', 'uploads/64852556d8cce71oZ45OrLjL.jpg', '64852556d8cce71oZ45OrLjL.jpg', 460.00, 1, '21:37:26');
+(8, 'In voluptates illo v', 6, 'In dolores et nulla ', NULL, 'Ut corporis fugiat a', 'Maxime ipsa volupta', 'uploads/64852556d8cce71oZ45OrLjL.jpg', '64852556d8cce71oZ45OrLjL.jpg', 460.00, 1, '21:37:26'),
+(9, 'Grokking Algorithms: An Illustrated Guide for Programmers and Other Curious People', 6, 'Aditya Bhargava', 'First Edition', 'Grokking Algorithms is a fully illustrated, friendly guide that teaches you how to apply common algorithms to the practical problems you face every day as a programmer. You\'ll start with sorting and searching and, as you build up your skills in thinking algorithmically, you\'ll tackle more complex concerns such as data compression and artificial intelligence. Each carefully presented example includes helpful diagrams and fully annotated code samples in Python.', 'Algorithms', 'uploads/648a5993d877281HwgKQ39lS.jpg', '648a5993d877281HwgKQ39lS.jpg', 20.00, 0, '20:21:39');
 
 -- --------------------------------------------------------
 
@@ -74,7 +75,7 @@ CREATE TABLE `Person` (
   `is_admin` tinyint(1) NOT NULL DEFAULT 0,
   `password` varchar(255) NOT NULL,
   `uuid` varchar(50) DEFAULT NULL,
-  `password_timestamp` time DEFAULT NULL,
+  `password_timestamp` datetime DEFAULT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -85,9 +86,9 @@ CREATE TABLE `Person` (
 INSERT INTO `Person` (`person_id`, `first_name`, `last_name`, `phone`, `address`, `email`, `is_admin`, `password`, `uuid`, `password_timestamp`, `is_active`) VALUES
 (1, 'Anthony', 'Gutierrez', '1234567890', '', 'gutierrez.anthony@student.greenriver.edu', 1, '$2y$10$boGBB.E4M.YCHn6Y8SWN/e9648FsUSELUEUCjSEesFwBhozFDZL1e', 'de11eeea-09bf-11ee-90fe-f23c91a78bbf', NULL, 0),
 (2, 'Anthony', 'Gutierrez', '1234567890', '1234 Lane, Auburn, WA, 98001', 'gutierrezanthony715@gmail.com', 0, '$2y$10$83aknNASEMjI34jXn5ILLuDgJGhYPYZ4PoFk550VIK4Ml/ltP.1Qq', '1dda9ce6-0a04-11ee-90fe-f23c91a78bbf', NULL, 0),
-(6, 'Mehdi', 'Jokar', '2065656710', '608 39Th Ave SW, Apt D20', 'jokar.mehdi2@gmail.com', 0, '$2y$10$rX1A1kT3ipm744HuqPiDDu5G78gRFSNrAO8mCDRUcSIIbO.OpNtS6', 'cf9f4b66-0753-11ee-90fe-f23c91a78bbf', NULL, 1),
+(6, 'Mehdi', 'Jokar', '2065656710', '608 39Th Ave SW', 'jokar.mehdi2@gmail.com', 0, '$2y$10$rX1A1kT3ipm744HuqPiDDu5G78gRFSNrAO8mCDRUcSIIbO.OpNtS6', 'cf9f4b66-0753-11ee-90fe-f23c91a78bbf', NULL, 1),
 (7, 'Galvin', 'Byrd', '(136) 964-6114', 'Eligendi beatae quis', 'qemynof@mailinator.com', 0, '$2y$10$ERLdCn8HZy9IsNQHF4ijvuH4uiSeh4hGsC6U6LYr/CxioRWc244Bm', '719886ee-08bf-11ee-90fe-f23c91a78bbf', NULL, 0),
-(8, 'Mehdi', 'Jokar', '905 974-3204', 'Alias et rerum Nam r', 'Jokar.Mehdi@student.greenriver.edu', 1, '$2y$10$UVlw7z2m0rqQFAH.5ysm7u7kxnudb3nqkAFBt6LTAr2fifriwBkc2', 'f974ad62-08bf-11ee-90fe-f23c91a78bbf', NULL, 1);
+(8, 'Mehdi', 'Jokar', '905 974-3204', 'Alias et rerum Nam r', 'Jokar.Mehdi@student.greenriver.edu', 1, '$2y$10$0LOhXlKCm0ionIZZqD0cwuzaCqLRZk6tVjyoUppWvjD47/2uwgRlG', 'ee103d08-0b31-11ee-90fe-f23c91a78bbf', '2023-06-15 04:20:47', 1);
 
 --
 -- Indexes for dumped tables
@@ -114,7 +115,7 @@ ALTER TABLE `Person`
 -- AUTO_INCREMENT for table `Book`
 --
 ALTER TABLE `Book`
-  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `Person`
